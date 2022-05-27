029b6aac177de32eae54ab074063d139085ab8a4
ozgur <ozgurkurucan337@gmail.com>
2022-05-27 18:01:06 +0300

    win download link update

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,15 @@ $(function () {
     // init feather icons
     feather.replace();
 });
+
+
+//detect os
+
+window.onload = () => {
+
+    const downloadLink = document.getElementById('download-link')
+
+    if (navigator.appVersion.indexOf("Win") != -1) downloadLink.setAttribute('href','https://github.com/yessGlory17/programmer-browser/releases/download/pre-release-0.1.1/ProgrammerBrowser.Setup.4.6.0.exe');
+    //if (navigator.appVersion.indexOf("Linux") != -1) downloadLink.setAttribute('href','');
+
+}

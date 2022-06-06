ea538fb50a9cd124dff94776b012060990114608
Özgür <52113663+yessGlory17@users.noreply.github.com>
2022-06-07 02:42:12 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ window.onload = () => {
 
     const downloadLink = document.getElementById('download-link')
 
-    if (navigator.appVersion.indexOf("Win") != -1) downloadLink.setAttribute('href','https://github.com/yessGlory17/programmer-browser/releases/download/pre-release-0.1.4/ProgrammerBrowser.Setup.4.6.0.exe');
+    if (navigator.appVersion.indexOf("Win") != -1) downloadLink.setAttribute('href','https://github.com/yessGlory17/programmer-browser/releases/download/v0.1.8/ProgrammerBrowser-Setup-0.1.8.exe');
     //if (navigator.appVersion.indexOf("Linux") != -1) downloadLink.setAttribute('href','');
 
 }
